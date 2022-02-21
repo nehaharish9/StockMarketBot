45ece071cfc42b05b6c83367f2595476b21e6922
Neha Harish <63835149+nehaharish9@users.noreply.github.com>
2022-02-21 18:39:32 +0530

    Add files via upload

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABotAllFeatures.sln -p:DeployOnBuild=true -p:PublishProfile=.\stockmarket-bot-Web-Deploy.pubxml -p:Password=vYWqTYfiziq3XN8l1dyyfiH4cS7lojaF0pY3ahuFPj9jpprawsllntCiv4dY
+
